3f515b9bae50ec0962f74ffdda4cd91c5081d7ec
Vishad Jain <51223992+jvishad@users.noreply.github.com>
2020-12-12 23:31:18 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include<math.h>
+#include<climits>
+#include<queue>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbb pop_back()
+#define mp make_pair
+#define pl pair<ll,ll>
+#define vl vector<ll>
+#define mod 1000000007
+#define INF 1000000000
+
+
+void solve() {
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    cout<<fixed<<setprecision(10);
+    int t = 1;
+//    cin>>t;
+//    int tc = 0;
+    while(t--) {
+        solve();
+
+    }
+}
+//those who do not remember past are condemned to repeat it.
