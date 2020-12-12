00c155347a4a07387eb60f547b7989cae38a0947
Vishad Jain <51223992+jvishad@users.noreply.github.com>
2020-12-12 23:28:00 +0530

    Add files via upload

---
+++ Number theory/fib sum.cpp
@@ -0,0 +1,49 @@
+//   Number theory total sum in between two fibonacci numbers
+
+#include <bits/stdc++.h>
+using namespace std;
+#define c 1000000007
+void multiply(int m1[2][2],int m2[2][2]){
+	int first = m1[0][0]*m2[0][0] + m1[0][1]*m2[1][0];
+	int second = m1[0][0]*m2[0][1] + m1[0][1]*m2[1][1];
+	int third = m1[1][0]*m2[0][0] + m1[1][1]*m2[1][0];
+	int fourth = m1[1][0]*m2[0][1] + m1[1][1]*m2[1][1];
+	m1[0][0] = first;
+	m1[0][1] = second;
+	m1[1][0] = third;
+	m1[1][1] = fourth;
+}
+
+void power(int a[][2],int n){
+	int m[2][2] = {{1,1},{1,0}};
+	if(n==0|| n==1){
+		return;
+	}
+	power(a,n/2);
+
+	multiply(a,a);
+	if(n%2){
+
+		multiply(a,m);
+	}
+	return;
+}
+
+int fib(int n){
+	int a[2][2]  = {{1,1},{1,0}};
+	if(n==0){
+		return 0;
+	}
+
+	power(a,n-1);
+
+	return a[0][0];
+}
+
+int main() {
+	// your code goes here
+	int n = 8 , m = 5;
+	int ans = (fib(n+1)%c - fib(m)%c)%c;
+	cout<<ans<<endl;
+	return 0;
+}
